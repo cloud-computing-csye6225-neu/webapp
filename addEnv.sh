3d1c622608eb5f2aadae44b847c1c781af591858
poosarla-sri-vijay-kalki <srivijaykalki@gmail.com>
2024-02-20 20:15:15 -0500

    [ADD]: add env file

---
+++ addEnv.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+# Create a new .env file
+touch .env
+
+DB_URL=$1
+DB_USER=$2
+DB_PASSWORD=$3
+
+# Append variables to .env file
+echo "DB_HOST=$DB_URL" >> .env
+echo "DB_USER=$DB_USERNAME" >> .env
+echo "DB_PASSWORD=$DB_PASSWORD" >> .env
+
+# Move .env file to /tmp/
+echo "Moving .env file to /tmp/"
+mv .env /tmp/
+
