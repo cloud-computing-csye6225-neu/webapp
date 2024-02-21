0b3ae21290eb19b6bd984ca31c55494a3326227b
poosarla-sri-vijay-kalki <srivijaykalki@gmail.com>
2024-02-21 13:35:37 -0500

    [UPDATE]: add env file

--- env-setup.sh
+++ env-setup.sh
@@ -1,22 +1,22 @@
 #!/bin/bash
 
 # Create a new .env file
-# touch .env
-# echo "$1"
-# echo "$2"
-# echo "$3"
-# DB_URL="$1"
-# DB_USERNAME="$2"
-# DB_PASSWORD="$3"
+touch .env
+echo "$1"
+echo "$2"
+echo "$3"
+DB_URL=$DB_URL
+DB_USERNAME=$DB_USERNAME
+DB_PASSWORD=$DB_PASSWORD
 
-# # Append variables to .env file
-# echo "DB_HOST=$DB_URL" >> .env
-# echo "DB_USER=$DB_USERNAME" >> .env
-# echo "DB_PASSWORD=$DB_PASSWORD" >> .env
+# Append variables to .env file
+echo "DB_HOST=$DB_URL" >> .env
+echo "DB_USER=$DB_USERNAME" >> .env
+echo "DB_PASSWORD=$DB_PASSWORD" >> .env
 
-# # Move .env file to /tmp/
-# echo "Moving .env file to /tmp/"
-# mv .env /tmp/
+# Move .env file to /tmp/
+echo "Moving .env file to /tmp/"
+sudo mv .env /opt/
 
 # updating the permissions of the jar to user and password
 sudo mv /tmp/healthCheckAPI-0.0.1-SNAPSHOT.jar /opt/ && sudo chown csye6225: /opt/healthCheckAPI-0.0.1-SNAPSHOT.jar
