2eeead32d87a6a14138287c3caf387ebe59b4b49
poosarla-sri-vijay-kalki <srivijaykalki@gmail.com>
2024-02-21 16:11:01 -0500

    [TEST]

--- packer/scripts/env-setup.sh
+++ packer/scripts/env-setup.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 
 # Create a new .env file
-touch .env
 DB_URL=$DB_URL
 DB_USERNAME=$DB_USERNAME
 DB_PASSWORD=$DB_PASSWORD
@@ -11,16 +10,18 @@ echo "DB_HOST=$DB_URL" >> .env
 echo "DB_USER=$DB_USERNAME" >> .env
 echo "DB_PASSWORD=$DB_PASSWORD" >> .env
 
-# Move .env file to /tmp/
-echo "Moving .env file to /tmp/"
-sudo mv .env /opt/ && sudo chown csye6225: /opt/.env
+# Move .env file to /opt/
+echo "Moving .env file to /opt/"
+sudo mv .env /opt/
 
 # setting enforce to 0
-sudo setenforce 0
+# sudo setenforce 0
 
 # updating the permissions of the jar to user and password
-sudo mv /tmp/healthCheckAPI-0.0.1-SNAPSHOT.jar /opt/ && sudo chown csye6225: /opt/healthCheckAPI-0.0.1-SNAPSHOT.jar
-sudo chown csye6225: /tmp/csye6225.service && sudo mv /tmp/csye6225.service /etc/systemd/system
+sudo chown csye6225: /tmp/healthCheckAPI-0.0.1-SNAPSHOT.jar
+sudo mv /tmp/healthCheckAPI-0.0.1-SNAPSHOT.jar /opt/
+sudo chown csye6225: /tmp/csye6225.service 
+sudo mv /tmp/csye6225.service /etc/systemd/system
 
 # add the service to systemd
 sudo systemctl daemon-reload
