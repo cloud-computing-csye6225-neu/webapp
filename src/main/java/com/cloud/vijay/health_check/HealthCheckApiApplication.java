6834a6232226dfb0c0fbd057a8bef53027d7cce3
poosarla-sri-vijay-kalki <srivijaykalki@gmail.com>
2024-03-18 22:11:54 -0400

    [UPDATE]: log4j config

--- src/main/java/com/cloud/vijay/health_check/HealthCheckApiApplication.java
+++ src/main/java/com/cloud/vijay/health_check/HealthCheckApiApplication.java
@@ -2,12 +2,17 @@ package com.cloud.vijay.health_check;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 
 @SpringBootApplication
 public class HealthCheckApiApplication {
 
+    private static final Logger LOGGER = LogManager.getLogger(HealthCheckApiApplication.class);
     public static void main(String[] args) {
+        LOGGER.info("Debug message");
         SpringApplication.run(HealthCheckApiApplication.class, args);
+
     }
 
 }
