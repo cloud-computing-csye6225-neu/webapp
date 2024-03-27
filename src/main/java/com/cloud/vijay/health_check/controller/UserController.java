564c5bbd8b91947a32e8c166e03ccd778892d29c
poosarla-sri-vijay-kalki <srivijaykalki@gmail.com>
2024-03-27 13:50:41 -0400

    [UPDATE]: Update Logs and exceptions

--- src/main/java/com/cloud/vijay/health_check/controller/UserController.java
+++ src/main/java/com/cloud/vijay/health_check/controller/UserController.java
@@ -3,8 +3,6 @@ package com.cloud.vijay.health_check.controller;
 import com.cloud.vijay.health_check.dto.UpdateUserDTO;
 import com.cloud.vijay.health_check.dto.UserDTO;
 import com.cloud.vijay.health_check.exception.BadRequestException;
-import com.cloud.vijay.health_check.model.VerificationToken;
-import com.cloud.vijay.health_check.service.PubSubService;
 import com.cloud.vijay.health_check.service.UserService;
 import com.cloud.vijay.health_check.service.VerificationTokenService;
 import com.cloud.vijay.health_check.util.CommonUtil;
@@ -28,9 +26,8 @@ public class UserController {
     @PostMapping(path = "/v1/user")
     public ResponseEntity<UserDTO> addUser(@RequestBody @Valid UserDTO userDTO, HttpServletRequest request) throws Exception {
         userDTO = userService.addUser(userDTO, request);
-        LOGGER.info("Successfully Created User");
-        verificationTokenService.sendVerification(userDTO);
-        LOGGER.info("sent email successfully to the user");
+        LOGGER.info("Successfully Created User :"+userDTO.getUserName());
+        verificationTokenService.sendVerificationMail(userDTO);;
         return new ResponseEntity<>(userDTO, HttpStatus.CREATED);
     }
 
@@ -38,12 +35,12 @@ public class UserController {
     public ResponseEntity<UserDTO> getSelfDetails(HttpServletRequest request) throws Exception {
 
         if (CommonUtil.hasRequestBody(request)) {
-            LOGGER.error("Bad Request");
+            LOGGER.error("A bad Request request received for the path : /V1/user/self");
             throw new BadRequestException();
         }
 
         UserDTO userDTO = userService.getUserDTO(request);
-        LOGGER.info("Fetched User Details");
+        LOGGER.info("Fetched User Details of the user : "+userDTO.getUserName());
         return new ResponseEntity<>(userDTO, HttpStatus.OK);
     }
 
@@ -51,29 +48,39 @@ public class UserController {
     public ResponseEntity<Void> updateSelfDetails(@RequestBody @Valid UpdateUserDTO updateUserDTO, HttpServletRequest request) throws Exception {
 
         userService.updateUser(updateUserDTO, request);
-        LOGGER.info("Successfully updated the user");
+        LOGGER.info("Successfully updated the user :"+updateUserDTO.getFirstName() + " " + updateUserDTO.getLastName());
         return new ResponseEntity<>(HttpStatus.NO_CONTENT);
     }
 
     @RequestMapping(path = "/v1/user", method = {RequestMethod.GET, RequestMethod.PUT, RequestMethod.PATCH, RequestMethod.DELETE, RequestMethod.HEAD, RequestMethod.OPTIONS, RequestMethod.TRACE})
     public ResponseEntity<Void> unSupportedMethodsForUser() {
-        LOGGER.error("Requested method is not unsupported");
+        LOGGER.error("Requested method is not unsupported : /v1/user");
         return new ResponseEntity<Void>(HttpStatus.METHOD_NOT_ALLOWED);
     }
 
     @RequestMapping(path = "/v1/user/self", method = {RequestMethod.POST, RequestMethod.PATCH, RequestMethod.DELETE, RequestMethod.HEAD, RequestMethod.OPTIONS, RequestMethod.TRACE})
     public ResponseEntity<Void> unSupportedMethodsForSelf() {
-        LOGGER.error("Requested method is not unsupported");
+        LOGGER.error("Requested method is not unsupported for the path : /v1/user/self");
         return new ResponseEntity<Void>(HttpStatus.METHOD_NOT_ALLOWED);
     }
 
-    @RequestMapping(path = "/validateAccount", method = {RequestMethod.POST, RequestMethod.GET})
-    public ResponseEntity<Void> validateAccount(@RequestParam  String token){
+    @RequestMapping(path = "/validateAccount", method = { RequestMethod.GET})
+    public ResponseEntity<String> validateAccount(@RequestParam  String token) throws Exception {
+        if(token.isEmpty()) {
+            LOGGER.error("Received invalid token :" + token);
+            throw new BadRequestException();
+        }
         Boolean isVerified = verificationTokenService.isVerified(token);
-        if(isVerified)
-            return  new ResponseEntity<>(HttpStatus.OK);
-        else
-            return new ResponseEntity<>(HttpStatus.UNPROCESSABLE_ENTITY);
+        if (isVerified) {
+            return ResponseEntity.ok("Account is verified successfully");
+        } else {
+            return ResponseEntity.badRequest().body("Account verification failed");
+        }
+    }
 
+    @RequestMapping(path = "/validateAccount", method = {RequestMethod.PUT,RequestMethod.POST, RequestMethod.PATCH, RequestMethod.DELETE, RequestMethod.HEAD, RequestMethod.OPTIONS, RequestMethod.TRACE})
+    public ResponseEntity<Void> unSupportedMethodsForVerification() {
+        LOGGER.error("Requested method is not unsupported path : /validateAccount");
+        return new ResponseEntity<Void>(HttpStatus.METHOD_NOT_ALLOWED);
     }
 }
